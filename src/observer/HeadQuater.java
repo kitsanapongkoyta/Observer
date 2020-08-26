cfea27dfbf2b3347a0fc1d095ba60462218bd94b
kitsanapongkoyta <43924638+kitsanapongkoyta@users.noreply.github.com>
2020-08-26 23:03:04 +0700

    Create HeadQuater.java

---
+++ src/observer/HeadQuater.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author sarun
+ */
+public class HeadQuater extends Observable {
+
+    private int someData;
+
+    public void setSomeData(int aData) {
+    someData = aData;
+    setChanged();
+    notifyObservers();
+    }
+    public int getSomeData() {
+    return someData;
+    }
+}
